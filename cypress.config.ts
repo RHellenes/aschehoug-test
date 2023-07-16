9c14d6e8bf8ba7ca61adfddc3f9977c2802e005a
René Hellenes <rene.hellenes@gmail.com>
2023-07-16 11:52:18 +0200

    Re trigger build

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  component: {
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+      viteConfig: {
+        server: {
+          host: '127.0.0.1'
+        }
+      }
     },
-  },
+  }
 });
